83c1d169dde038bf76c47cbcb79fe896b4b7aacd
Stefan Lindahl <stefan.lindahl87@gmail.com>
2018-09-17 13:33:07 +0200

    Added console project

---
+++ securedating/securedating/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace securedating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial commit, skapa din egen branch!");
+        }
+    }
+}
